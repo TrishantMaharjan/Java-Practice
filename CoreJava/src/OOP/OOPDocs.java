596489e31f89c5886e87ec399bbcf88ca9607413
Trishant Maharjan <trishant0002@gmail.com>
2022-12-26 16:53:25 +0545

    Class introduction

---
+++ CoreJava/src/OOP/OOPDocs.java
@@ -0,0 +1,29 @@
+package OOP;
+
+public class OOPDocs {
+	public static void main(String[] args) {
+		/*
+		 * ========== OOP (Object Oriented Programming) ==========
+		 * 1) Class and Object
+		 * 2) Encapsulation
+		 * 3) Inheritance
+		 * 4) Polymorphism
+		 * 5) Abstraction
+		 * 		a) Abstract Class
+		 * 		b) Interface
+		 * 
+		 * # Class:
+		 * 		- Used to represent real world entity/things.
+		 * 		- Collection or group of objects having similar behaviors.
+		 * 		- Blueprint/template of object.
+		 * 		- Contains properties and methods.
+		 * 		- Is user defined or reference data type.
+		 * 
+		 * Syntax:
+		 * 		class class_name {
+		 *			//properties/features
+		 *			//methods/functions/behaviors/responsibilities
+		 * 		} 
+		 */
+	}
+}
