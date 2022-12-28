c1645d7ef4944fce080f8c975962f5321d540273
Trishant Maharjan <trishant0002@gmail.com>
2022-12-28 17:43:51 +0545

    Added constructor docs and example

---
+++ CoreJava/src/Constructor/ConstructorDocs.java
@@ -0,0 +1,19 @@
+package Constructor;
+
+public class ConstructorDocs {
+	/*
+	 * ========== Constructor ==========
+	 * - Used to create object in java
+	 * - Special type of method which has the same name as class name is known as constructor
+	 * 
+	 * Types:-
+	 * 	1) Default constructor
+	 * 		- Used to create object and set default values to it
+	 * 	2) Parameterized constructor
+	 * 		- Used to create object and set dynamic values to it
+	 * 		- Syntax:
+	 * 			class_name(){
+	 * 				//set dynamic values to it
+	 * 			}
+	 */
+}
---
+++ CoreJava/src/Constructor/User.java
@@ -0,0 +1,40 @@
+package Constructor;
+
+public class User {
+	
+	String username;
+	String password;
+	
+	// Default constructor
+	User(){
+		username = "Admin";
+		password = "Admin";
+	}
+	
+	// Parameterized construction
+	/*
+	User(String un, String psw){
+		username = un;
+		password = psw;
+	}
+	*/
+	
+	User(String username, String password){
+		this.username = username;
+		this.password = password;
+		// 'this' keyword represents current object
+	}
+	
+	// Same class can have multiple constructor, but must have different arguments in parameter. This is called constructor overloading.
+	
+	void print() {
+		System.out.println("Username = "+username);
+		System.out.println("Password = "+password);
+	}
+	
+	public static void main(String[] args) {
+		// User u = new User();
+		User u = new User("Name","Password");
+		u.print();
+	}
+}
