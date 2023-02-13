8756f0f6505cff8be6308323fa354d614c4c4b64
Trishant Maharjan <trishant0002@gmail.com>
2023-02-13 15:01:17 +0545

    Learnt about Mapping in tables (1to1, 1tomany, manytomany)

--- mvndemo/src/main/java/com/model/Employee.java
+++ mvndemo/src/main/java/com/model/Employee.java
@@ -1,9 +1,14 @@
 package com.model;
 
+import java.util.List;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
@@ -16,9 +21,30 @@ public class Employee {
 	private String fname;
 	private String lname;
 	private int salary;
+	
 	@OneToOne
 	@JoinColumn(name = "addressId")
 	private Address address;
+	
+	@OneToMany(mappedBy = "employee")
+	private List<Phone> phones;
+	
+	@ManyToMany
+	@JoinTable(name="emp_dept", joinColumns={@JoinColumn(name = "empId")}, inverseJoinColumns={@JoinColumn(name = "deptId")})
+	private List<Department> deptList;
+	
+	public List<Department> getDeptList() {
+		return deptList;
+	}
+	public void setDeptList(List<Department> deptList) {
+		this.deptList = deptList;
+	}
+	public List<Phone> getPhones() {
+		return phones;
+	}
+	public void setPhones(List<Phone> phones) {
+		this.phones = phones;
+	}
 	public int getId() {
 		return id;
 	}
--- mvndemo/src/main/java/com/model/MappingTest.java
+++ mvndemo/src/main/java/com/model/MappingTest.java
@@ -1,12 +1,16 @@
 package com.model;
 
+import java.util.Arrays;
+
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
 public class MappingTest {
 	public static void main(String[] args) {
-		oneToOne();
+		//oneToOne();
+		//manyToOne();
+		manyToMany();
 	}
 	
 	static void oneToOne() {
@@ -25,6 +29,91 @@ public class MappingTest {
 		emp.setLname("DummyLname");
 		emp.setSalary(10000);
 		emp.setAddress(adr);
+		
+		sess.save(emp);
+		sess.getTransaction().commit();
+		sess.close();
+	}
+	
+	static void manyToOne() {
+		SessionFactory sf = new Configuration().configure().buildSessionFactory();
+		Session sess = sf.openSession();
+		sess.beginTransaction();
+		
+		Address adr = new Address();
+		adr.setCountry("Nepal");
+		adr.setState("Bagmati");
+		adr.setCity("Kathmandu");
+		sess.save(adr);
+		
+		Employee emp = new Employee();
+		emp.setFname("DummyFname");
+		emp.setLname("DummyLname");
+		emp.setSalary(10000);
+		emp.setAddress(adr);
+		
+		Phone p1 = new Phone();
+		p1.setNumber("123456789");
+		p1.setType("Ncell");
+		p1.setEmployee(emp);
+		
+		sess.save(p1);
+		
+		Phone p2 = new Phone();
+		p2.setNumber("987654321");
+		p2.setType("NTC");
+		p2.setEmployee(emp);
+		
+		sess.save(p2);
+		
+		emp.setPhones(Arrays.asList(p1,p2));
+		
+		sess.save(emp);
+		sess.getTransaction().commit();
+		sess.close();
+	}
+	
+	static void manyToMany() {
+		SessionFactory sf = new Configuration().configure().buildSessionFactory();
+		Session sess = sf.openSession();
+		sess.beginTransaction();
+		
+		Address adr = new Address();
+		adr.setCountry("Nepal");
+		adr.setState("Bagmati");
+		adr.setCity("Kathmandu");
+		sess.save(adr);
+		
+		Employee emp = new Employee();
+		emp.setFname("DummyFname");
+		emp.setLname("DummyLname");
+		emp.setSalary(10000);
+		emp.setAddress(adr);
+		
+		Phone p1 = new Phone();
+		p1.setNumber("123456789");
+		p1.setType("Ncell");
+		p1.setEmployee(emp);
+		
+		sess.save(p1);
+		
+		Phone p2 = new Phone();
+		p2.setNumber("987654321");
+		p2.setType("NTC");
+		p2.setEmployee(emp);
+		
+		sess.save(p2);
+		
+		Department d1 = new Department();
+		d1.setName("IT");
+		sess.save(d1);
+		
+		Department d2 = new Department();
+		d2.setName("Finance");
+		sess.save(d2);
+		
+		emp.setPhones(Arrays.asList(p1,p2));
+		emp.setDeptList(Arrays.asList(d1,d2));
 		
 		sess.save(emp);
 		sess.getTransaction().commit();
